933881344c3ac738c8c9a80fa3abe91514fa122e
Ray Mazurik <62910772+rjmazurik@users.noreply.github.com>
2022-02-28 16:43:08 -0700

    Update Apache2Install.sh

--- Apache2Install.sh
+++ Apache2Install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+dpkg --configure -a
+apt-get -y update
+# install Apache2
 apt-get -y install apache2
 apt-get -y install mysql-server
 apt-get -y install php libapache2-mod-php php-mysql
