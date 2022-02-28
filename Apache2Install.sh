09a613a17615f8759747f179458a5d16c16d2009
Ray Mazurik <62910772+rjmazurik@users.noreply.github.com>
2022-02-28 16:41:36 -0700

    Update Apache2Install.sh

--- Apache2Install.sh
+++ Apache2Install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo apt-get -y update
-# install Apache2
-sudo apt-get install -y apache2
+apt-get -y install apache2
+apt-get -y install mysql-server
+apt-get -y install php libapache2-mod-php php-mysql
 # write some HTML
 echo \<center\>\<h1\>My LB Test on $HOSTNAME\</h1\>\<br/\>\</center\> > /var/www/html/index.html
-
 # restart Apache
 apachectl restart
