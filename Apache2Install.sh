ac6b37fa40c336ca9be77167c979b499f9aea74d
Ray Mazurik <62910772+rjmazurik@users.noreply.github.com>
2022-02-28 17:22:04 -0700

    Update Apache2Install.sh

--- Apache2Install.sh
+++ Apache2Install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get -y update
+sudo apt-get -y update
 # install Apache2
-apt-get -y install apache2
+sudo apt-get -y install apache2
 # write some HTML
 sudo bash -c 'echo \<center\>\<h1\>My LB Test on $HOSTNAME\</h1\>\<br/\>\</center\> > /var/www/html/index.html'
 # restart Apache
-apachectl restart
+sudo apachectl restart
